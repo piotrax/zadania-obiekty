65919aaca08e4faf42ee7467f494a77fef444579
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-14 14:26:28 +0200

    Update app.js

--- 1-zadania/js/app.js
+++ 1-zadania/js/app.js
@@ -10,9 +10,27 @@ const prod2 = {
   weight: 1.4
 }
 
-function skrypt(){
-document.getElementById("prod1").textContent = prod1.name;
-document.getElementById("prod2").textContent = prod2.name;
-document.getElementById("suma").textContent = prod1.price + prod2.price;
-document.getElementById("waga").textContent = (prod1.weight + prod2.weight).toFixed(1)
+const currentUser = {
+  name: "Jan",
+  surname: "Kowalski",
+  email: "jankowalski@email.com",
+  www: "www.jkowalski.pl",
+  userType: "user",
+  show(){
+    console.log("Imię: " + this.name);
+    console.log("Nazwisko: " + this.surname);
+    console.log("email: " + this.email);
+    console.log("www: " + this.www);
+    console.log("typ użytkownika: " + this.userType);
+  }
 }
+
+function skrypt(){
+  document.getElementById("prod1").textContent = prod1.name;
+  document.getElementById("prod2").textContent = prod2.name;
+  document.getElementById("suma").textContent = prod1.price + prod2.price;
+  document.getElementById("waga").textContent = (prod1.weight + prod2.weight).toFixed(1)
+  currentUser.show();
+}
+
+
