6a9f1fdc27cdccef055440c4926bc4d72f21db1f
Peter <56226439+piotrax@users.noreply.github.com>
2020-09-15 18:10:00 +0200

    Update app.js

---
+++ 2-zadania/js/app.js
@@ -0,0 +1,32 @@
+const spaceShip = {
+    name: "Enterprise",
+    currentLocation: "Earth",
+    flyDistance: 0,
+    flyTo(place, distance){
+      this.currentLocation = place;
+      this.flyDistance =+ distance;
+    },
+    showInfo(){
+      console.log("Informacje o statku:\n--------------------\nStatek " + this.name);
+      console.log("doleciał do miejsca " + this.currentLocation);
+    },
+    meetClingon(){
+        let x = 0;
+        for(let i = 0; i < 100; i++){
+          x += Math.floor((Math.random() * 2));
+        }
+        if (x>50){
+          console.log("Statek " + this.name + " będący w okolicy " + this.currentLocation + " zwycięsko wyszedł ze spotkania z Klingonami")
+
+        } 
+        else {
+          console.log("Statek " + this.name + " będący w okolicy " + this.currentLocation + " został pokonany przez Klingonów")
+        } 
+  }
+}
+
+function skrypt(){
+    spaceShip.showInfo();
+    spaceShip.meetClingon();
+  }
+  
